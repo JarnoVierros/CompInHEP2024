9cd2cd677f79bb354932bc7dc4358e5129585135
JarnoVierros <vierros.jarno@gmail.com>
2024-02-02 02:37:45 +0200

    Add answer to bonus problem

--- Answers/Answer_2/get_totrecorded.py
+++ Answers/Answer_2/get_totrecorded.py
@@ -10,3 +10,22 @@ with open("brilcalc.log", "r") as file:
 totrecorded = totrecorded/1000 #unit conversion
 
 print(f"Summary luminosity: {totrecorded:.1f} (fb)^(-1)")
+
+#Problem 3 here:
+
+total = 0
+
+with open("brilcalc.log", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        line = line.strip()
+        if line[0] == "|" and line[-1] == "|":
+            if "totrecorded" in line: break
+            if "recorded" in line: continue
+            values = line[1:-1].split("|")
+            total += float(values[-1])
+
+total = total/1000
+
+print(f"Summary luminosity confirmation: {total:.1f} (fb)^(-1)")
+print(f"Difference from original: {totrecorded-total} (fb)^(-1)")
